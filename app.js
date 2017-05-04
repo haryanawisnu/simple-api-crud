5bcba1d9dcdd4654f79e3a12887fd86aa615f48a
haryanawisnu <arya.wisnu05@gmail.com>
2017-05-04 11:05:08 +0700

    done testing CRUD collection foods

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
+var app = express();
+
 //routes
 var foods = require('./routes/foods');
 var restaurants = require('./routes/restaurants');
 
+//Body Parser
+var bodyParser = require('body-parser');
+
 //DB Mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/livecode2');
+
+//Init Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 //HTTP Route
 app.use('/foods', foods);
